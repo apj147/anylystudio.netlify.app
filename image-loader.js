d10c8d8cdab15b5215ed4e5e90c09f08ec4141c7
Your Name <your-github-email@example.com>
2026-03-27 07:23:11 -0500

    Revert "fix: netlify image loader use relative path to avoid double-hop"
    This reverts commit f59f915052734601432409dc45c24b28282acb1c.

--- image-loader.js
+++ image-loader.js
@@ -1,6 +1,6 @@
 export default function netlifyImageLoader({ src, width, quality }) {
   const params = new URLSearchParams({
-    url: src.startsWith('http') ? src : src,
+    url: src.startsWith('http') ? src : `https://anylystudio.com${src}`,
     w: width,
     q: quality || 75,
   })
